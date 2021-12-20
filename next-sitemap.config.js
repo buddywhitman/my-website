a7bd95aae7426e1e44bc1afaf2075fef07288c54
Pulkit Kumar <39520468+buddywhitman@users.noreply.github.com>
2021-12-20 21:55:42 +0530

    updated sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 const NextSitemapConfig = {
-  siteUrl: "https://daggy.tech",
+  siteUrl: "https://buddywhitman.vercel.app/",
   generateRobotsTxt: true,
 };
 
