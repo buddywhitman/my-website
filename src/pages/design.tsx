a5531c14d6b520305df78a0983d82c40e98b01a2
Pulkit Kumar <39520468+buddywhitman@users.noreply.github.com>
2021-12-23 21:28:48 +0530

    init design

--- src/pages/design.tsx
+++ src/pages/design.tsx
@@ -1,1 +1,42 @@
+import {
+  Box,
+  Heading,
+  Text,
+  useColorModeValue,
+  VisuallyHidden,
+  Icon,
+  Tooltip,
+  Input,
+  FormControl,
+  FormLabel,
+  Textarea,
+  Flex,
+} from "@chakra-ui/react";
+import Link from "next/link";
+const Design = () => {
+  const bl = useColorModeValue("brand.400", "brand.600");
+  return (
+    <Box p={{ base: 1, md: 8 }}>
+      <Heading as="h2" size="2xl">
+        <Text
+          as="span"
+          position="relative"
+          _after={{
+            content: "''",
+            width: "full",
+            height: "25%",
+            position: "absolute",
+            bottom: 1,
+            left: 0,
+            bg: bl,
+            zIndex: -1,
+          }}
+        >
+          Design
+        </Text>
+      </Heading>
+          </Box>
+  );
+};
 
+export default Contact;
