33debe5bf1441efd044ad7325c131dc8529c9488
Pulkit Kumar <39520468+buddywhitman@users.noreply.github.com>
2021-12-24 15:42:08 +0530

    made the home redirect work in link

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,5 @@
 import { Box, Button, Image } from "@chakra-ui/react";
-
+import Link from "next/link";
 import MotionBox from "components/motion/Box";
 
 const Page404 = () => {
@@ -19,7 +19,9 @@ const Page404 = () => {
 
       <Box marginY={4}>
         <Box textAlign="center" marginTop={4}>
-          <Button size="lg">Home</Button>
+          <Link href="/" passHref>
+            <Button size="lg">Home</Button>
+          </Link>  
         </Box>
         <a href="https://www.freepik.com/vectors/computer">
           Computer vector created by jcomp - www.freepik.com
