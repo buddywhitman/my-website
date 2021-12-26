e07766048eafa981acfcd3cc8862eaecee19b2c6
Restyled.io <commits@restyled.io>
2021-12-26 12:33:24 +0000

    Restyled by prettier

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,29 +1,29 @@
 /** @type {import('next-seo').DefaultSeoProps} */
 const defaultSEOConfig = {
-  title : "buddywhitman's Portfolio",
-  titleTemplate : "%s | buddywhitman",
-  defaultTitle : "buddywhitman",
-  description :
+  title: "buddywhitman's Portfolio",
+  titleTemplate: "%s | buddywhitman",
+  defaultTitle: "buddywhitman",
+  description:
+    "Personal website that showcases my best work, tells you about me and has some fun eastern eggs",
+  canonical: "https://buddywhitman.vercel.app",
+  openGraph: {
+    url: "https://buddywhitman.vercel.app",
+    title: "buddywhitman's Portfolio",
+    description:
       "Personal website that showcases my best work, tells you about me and has some fun eastern eggs",
-  canonical : "https://buddywhitman.vercel.app",
-  openGraph : {
-    url : "https://buddywhitman.vercel.app",
-    title : "buddywhitman's Portfolio",
-    description :
-        "Personal website that showcases my best work, tells you about me and has some fun eastern eggs",
-    images : [
+    images: [
       {
-        url : "https://www.daggy.tech/daggy_big.webp",
-        height : 1024,
-        width : 1024,
-        alt : "png",
+        url: "https://www.daggy.tech/daggy_big.webp",
+        height: 1024,
+        width: 1024,
+        alt: "png",
       },
     ],
-    site_name : "buddywhtitman's Portfolio",
+    site_name: "buddywhtitman's Portfolio",
   },
-  twitter : {
-    handle : "@desihippe",
-    cardType : "summary_large_image",
+  twitter: {
+    handle: "@desihippe",
+    cardType: "summary_large_image",
   },
 };
 
