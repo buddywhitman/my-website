4a4e724f05463c2bae1095e1a0b3379536647848
Pulkit Kumar <39520468+buddywhitman@users.noreply.github.com>
2023-05-10 19:26:31 +0530

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -84,12 +84,6 @@ class MyDocument extends Document {
           <link
             rel="stylesheet"
             type="text/css"
-            charSet="UTF-8"
-            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-          />
-          <link
-            rel="stylesheet"
-            type="text/css"
             href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
           />
         </Head>
