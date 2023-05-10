52cd76282ac3279f7cff9c87c66068c7153c406b
buddywhitman <pulkitindia5@gmail.com>
2023-05-10 16:20:03 +0530

    feat: update metadata and tech stack

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,23 +1,23 @@
 /** @type {import('next-seo').DefaultSeoProps} */
 const defaultSEOConfig = {
-  title: "buddywhitman's Portfolio",
+  title: "buddywhitman",
   titleTemplate: "%s | buddywhitman",
   defaultTitle: "buddywhitman",
   description: "Personal website that showcases my best work, tells you about me and has some fun eastern eggs",
   canonical: "https://buddywhitman.vercel.app",
   openGraph: {
     url: "https://buddywhitman.vercel.app",
-    title: "buddywhitman's Portfolio",
+    title: "buddywhitman",
     description: "Personal website that showcases my best work, tells you about me and has some fun eastern eggs",
     images: [
       {
-        url: "https://www.daggy.tech/daggy_big.webp",
+        url: "https://buddywhitman.vercel.app/daggy.png",
         height: 1024,
         width: 1024,
         alt: "png",
       },
     ],
-    site_name: "buddywhtitman's Portfolio",
+    site_name: "buddywhtitman",
   },
   twitter: {
     handle: "@desihippe",
