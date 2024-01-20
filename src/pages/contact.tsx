2d32c91d35d5b6183f878f2bcd80f8cc8c06fc4a
Pulkit Kumar <39520468+buddywhitman@users.noreply.github.com>
2024-01-20 11:35:28 +0530

    fix: contact form

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -57,7 +57,29 @@ const IconWrap = ({ icon, color, url, text }: IconWrapProps) => {
 
 const Contact = () => {
   const bl = useColorModeValue("brand.400", "brand.600");
-  return (
+  async function handleSubmit(event) {
+      event.preventDefault();
+      const formData = new FormData(event.target);
+  
+      formData.append("access_key", "ffedddbb-9617-479e-9e67-7cdf2d662ad9");
+  
+      const object = Object.fromEntries(formData);
+      const json = JSON.stringify(object);
+  
+      const response = await fetch("https://api.web3forms.com/submit", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+          },
+          body: json
+      });
+      const result = await response.json();
+      if (result.success) {
+          console.log(result);
+      }
+    }
+    return (
     <Box p={{ base: 1, md: 8 }}>
       <Heading as="h2" size="2xl">
         <Text
@@ -108,18 +130,20 @@ const Contact = () => {
           bg={bl}
           direction="column"
         >
-          <Heading size="lg">Contact Form</Heading>
-          <FormControl mb={3} id="email">
-            <FormLabel>Email address</FormLabel>
-            <Input type="email" />
-          </FormControl>
-          <FormControl mb={3} id="message">
-            <FormLabel>Message</FormLabel>
-            <Textarea />
-          </FormControl>
-          <ThemedButton bg="#ffffff" color="rgb(17, 17, 17)">
-            Submit
-          </ThemedButton>
+          <form onSubmit={handleSubmit}>
+            <Heading size="lg">Contact Form</Heading>
+            <FormControl mb={3} id="email">
+              <FormLabel>Email address</FormLabel>
+              <Input type="email" name="email" />
+            </FormControl>
+            <FormControl mb={3} id="message">
+              <FormLabel>Message</FormLabel>
+              <Textarea name="message" />
+            </FormControl>
+            <ThemedButton type="submit" bg="#ffffff" color="rgb(17, 17, 17)">
+              Submit
+            </ThemedButton>
+          </form>
         </Flex>
       </Flex>
     </Box>
