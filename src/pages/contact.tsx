0cde794fc648d0f1fa8ff580afc41cd5e36b1b0a
Pulkit Kumar <39520468+buddywhitman@users.noreply.github.com>
2024-01-20 12:44:06 +0530

    fix: prettier format

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -56,26 +56,25 @@ const IconWrap = ({ icon, color, url, text }: IconWrapProps) => {
 };
 
 const Contact = () => {
-const bl = useColorModeValue("brand.400", "brand.600");
-async function handleSubmit(event) {
-  event.preventDefault();
-  const formData = new FormData(event.target);
+    const bl = useColorModeValue("brand.400", "brand.600");
+    async function handleSubmit(event) {
+      event.preventDefault();
+      const formData = new FormData(event.target);
 
-  formData.append("access_key", "ffedddbb-9617-479e-9e67-7cdf2d662ad9");
+      formData.append("access_key", "ffedddbb-9617-479e-9e67-7cdf2d662ad9");
 
-  const object = Object.fromEntries(formData);
-  const json = JSON.stringify(object);
+      const object = Object.fromEntries(formData);
+      const json = JSON.stringify(object);
 
-  const response = await fetch("https://api.web3forms.com/submit", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-    body: json,
-  });
-  const result = await response.json();
-  }
+      const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: json,
+      });
+    }
   return (
     <Box p={{ base: 1, md: 8 }}>
       <Heading as="h2" size="2xl">
