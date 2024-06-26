74b09793e0fcaee5d83b013c7a54978f8ebc7f24
buddywhitman <pulkitindia5@gmail.com>
2024-06-26 22:44:55 +0530

    fix: get rid of eslint

--- next.config.js
+++ next.config.js
@@ -1,23 +1,22 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
+const MillionLint = require('@million/lint');
 const withPWA = require("next-pwa");
 
 /** @type {import('next').NextConfig} */
 module.exports = withPWA({
   swcMinify: true,
   pwa: {
-    disable:
-      process.env.NODE_ENV === "development",
-      // process.env.NODE_ENV === "preview" ||
-      // process.env.NODE_ENV === "production",
-      // delete two lines above to enable PWA in production deployment
-      // add your own icons to public/manifest.json 
-      // to re-generate manifest.json, you can visit https://tomitm.github.io/appmanifest/
+    disable: process.env.NODE_ENV === "development",
+    // process.env.NODE_ENV === "preview" ||
+    // process.env.NODE_ENV === "production",
+    // delete two lines above to enable PWA in production deployment
+    // add your own icons to public/manifest.json 
+    // to re-generate manifest.json, you can visit https://tomitm.github.io/appmanifest/
     dest: "public",
-    register: true,
+    register: true
   },
-  reactStrictMode: true,
+  reactStrictMode: true
 });
-{/*
+/*{
   "rewrites": [
     {
       "source": "/:path*",
@@ -30,4 +29,4 @@ module.exports = withPWA({
       "destination": "https://buddywhitman.vercel.app/blog"
     }
   ]
-*/}
+ }*/
